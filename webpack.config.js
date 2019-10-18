2a5aad5c6b1df9471bcc7ffeddd3f7d95497e293
Andrei Shablyka <kloppp0007@gmail.com>
2019-10-18 03:28:46 +0300

    feat: configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,35 @@ module.exports = {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
-      }
+          use: ['css-loader', 'sass-loader'],
+        }),
+      },
+      {
+        test: /.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/img'
+            }
+          }
+        ]
+      },
+      {
+        test: /.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/fonts'
+            }
+          }
+        ]
+      },
     ]
   },
   plugins: [
     new ExtractTextPlugin('style.css')
-  ]
+  ],
+  
 };
